44cc7b13cc0ef6e7f4cc969599a2ef9193774225
adelramadan55 <131725917+adelramadan55@users.noreply.github.com>
2023-05-17 02:54:10 +0300

    Add files via upload

---
+++ fifa15.sql
@@ -0,0 +1,167 @@
+select * from [fifa22].[dbo].[FIFA15]
+order by overall desc
+select short_name,  player_positions, overall, value_eur, wage_eur, age, club_name, nationality_name
+from [fifa22].[dbo].[FIFA15]
+where age between 20 and 29
+order by overall desc, age desc
+select top 50 *from [fifa22].[dbo].[FIFA15] where overall between 85 and 93 order by overall desc
+select short_name, club_name, value_eur, wage_eur, age, overall
+from [fifa22].[dbo].[FIFA15]
+where overall between 85 and 99 and age between 25 and 29
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'cam' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]tion = 'cb' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'cdm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'cf' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'cm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'gk' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'cam' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lam' and  overall between 78 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lb' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lcb' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lcm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'ldm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lf' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'ls' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lw' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'lwb' and  overall between 70 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'ram' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rb' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rcb' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rcm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rdm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'res' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rf' and  overall between 70 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rm' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rs' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rw' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'rwb' and  overall between 70 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'st' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select short_name, club_name, value_eur, wage_eur, age, overall, player_positions, club_position
+from [fifa22].[dbo].[FIFA15]
+where club_position = 'sub' and  overall between 80 and 99 and age between 20 and 34
+order by value_eur desc
+select top 20* from [fifa22].[dbo].[FIFA15]
+where value_eur between 30000000 and 150000000 and wage_eur between 150000 and 600000
+order by value_eur desc
+select  short_name, wage_eur, club_contract_valid_until
+from [fifa22].[dbo].[FIFA15]
+where league_name = 'Spain Primera Division'
+order by club_contract_valid_until
+ select MAX(value_eur) as MaxValueEur, nationality_name, short_name
+ from [fifa22].[dbo].[FIFA15]
+ where skill_moves >=4
+ group by nationality_name, short_name
+ order by MAX(value_eur) desc
+
+ select min(value_eur) as MaxValueEur, nationality_name, short_name
+ from [fifa22].[dbo].[FIFA15]
+ where skill_moves <=3
+ group by nationality_name, short_name
+ order by min(value_eur) desc
+ select max(wage_eur) as MaxValueEur, nationality_name, short_name
+ from [fifa22].[dbo].[FIFA15]
+ where overall between 85 and 99
+ group by nationality_name, short_name
+ order by max(wage_eur) desc
+ select  short_name,club_position, pace, shooting, passing, dribbling, defending, physic, attacking_crossing, attacking_finishing, attacking_heading_accuracy, attacking_short_passing, attacking_volleys, skill_curve
+ from [fifa22].[dbo].[FIFA15] fifa15
+ where pace between 80 and 99
+ order by pace desc
+ select * from [fifa22].[dbo].[FIFA22]
+ where overall between 85 and 99
+ order by overall desc
+ select * from [fifa22].[dbo].[FIFA22]
+ where wage_eur between 200000 and 700000
+ order by overall desc
+ select short_name, club_name, league_name, overall, value_eur, wage_eur
+ from [fifa22].[dbo].[FIFA22]
+ where value_eur between 25000000 and 150000000
+ order by overall desc
